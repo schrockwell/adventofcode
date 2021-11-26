d4fcbf8d851693e10600923606cce69627af3422
Rockwell Schrock <rockwell@DockYard-MacBook-Pro.local>
2021-11-26 14:09:02 -0500

    2020 day 3 elixir b

--- 2020/elixir/lib/advent_of_code/day_03.ex
+++ 2020/elixir/lib/advent_of_code/day_03.ex
@@ -2,9 +2,14 @@ defmodule AdventOfCode.Day03 do
   @behaviour AdventOfCode
 
   def run(input) do
-    answer_a = count_trees(input)
+    answer_a = count_trees(input, 3, 1)
 
-    answer_b = "todo"
+    slopes = [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+
+    answer_b =
+      Enum.reduce(slopes, 1, fn {x_inc, y_inc}, acc ->
+        acc * count_trees(input, x_inc, y_inc)
+      end)
 
     IO.puts("Answer A: #{answer_a}")
     IO.puts("Answer B: #{answer_b}")
@@ -12,17 +17,19 @@ defmodule AdventOfCode.Day03 do
     {to_string(answer_a), to_string(answer_b)}
   end
 
-  defp count_trees([_ | lines]) do
-    count_trees(lines, String.length(hd(lines)), 0, 0)
+  defp count_trees(lines, x_inc, y_inc) do
+    count_trees(lines, x_inc, y_inc, String.length(hd(lines)), 0, 0)
   end
 
-  defp count_trees([line | lines], line_length, x, tree_count) do
-    x = x + 3
-    char_index = rem(x, line_length)
+  defp count_trees(lines, x_inc, y_inc, line_length, x, tree_count) do
+    case Enum.split(lines, y_inc) do
+      {_, []} ->
+        tree_count
 
-    tree_inc = if(String.at(line, char_index) == "#", do: 1, else: 0)
-    count_trees(lines, line_length, x, tree_count + tree_inc)
+      {_, [line | _] = next_lines} ->
+        x = rem(x + x_inc, line_length)
+        tree_inc = if(String.at(line, x) == "#", do: 1, else: 0)
+        count_trees(next_lines, x_inc, y_inc, line_length, x, tree_count + tree_inc)
+    end
   end
-
-  defp count_trees([], _, _, tree_count), do: tree_count
 end
