35111c8682c52faa52d5a230862d2d440a70be1e
Rockwell Schrock <rockwell@DockYard-MBP.localdomain>
2021-12-10 11:28:17 -0500

    DRY things up a bit

--- 2021/elixir/lib/advent_of_code/day_10.ex
+++ 2021/elixir/lib/advent_of_code/day_10.ex
@@ -26,44 +26,41 @@ defmodule AdventOfCode.Day10 do
 
   defp validate_chunks(line, stack \\ [])
 
-  defp validate_chunks("", []) do
-    :ok
+  # We found a matching closing bracket for the opening bracket on the top of the stack
+  defp validate_chunks(<<close::binary-1, remaining::binary>>, [open | next_stack])
+       when <<open::binary-1, close::binary-1>> in ~w|() [] {} <>| do
+    validate_chunks(remaining, next_stack)
   end
 
+  # We found an opening bracket, so push it onto the stack
+  defp validate_chunks(<<open::binary-1, remaining::binary>>, stack) when open in ~w|( [ { <| do
+    validate_chunks(remaining, [open | stack])
+  end
+
+  # We found a closing bracket that does NOT match the latest opening bracket, so blow up
+  defp validate_chunks(<<illegal::binary-1, _remaining::binary>>, _stack) do
+    {:error, :illegal_char, illegal}
+  end
+
+  # We reached the end of the string with some opening brackets still on the stack
   defp validate_chunks("", [_ | _] = stack) do
     {:error, :incomplete, stack}
   end
 
-  defp validate_chunks(")" <> remaining, ["(" | next_stack]) do
-    validate_chunks(remaining, next_stack)
+  # We reached the end of the line with no brackets remaining - yay!
+  # (This never actually gets called with the challenge input, since all lines are invalid)
+  defp validate_chunks("", []) do
+    :ok
   end
 
-  defp validate_chunks("]" <> remaining, ["[" | next_stack]) do
-    validate_chunks(remaining, next_stack)
-  end
-
-  defp validate_chunks("}" <> remaining, ["{" | next_stack]) do
-    validate_chunks(remaining, next_stack)
-  end
-
-  defp validate_chunks(">" <> remaining, ["<" | next_stack]) do
-    validate_chunks(remaining, next_stack)
-  end
-
-  defp validate_chunks(<<char::binary-1, remaining::binary>>, stack) when char in ~w|( [ { <| do
-    validate_chunks(remaining, [char | stack])
-  end
-
-  defp validate_chunks(<<char::binary-1, _remaining::binary>>, _stack) do
-    {:error, :illegal_char, char}
-  end
-
+  # Part 1 result
   defp score_illegal_chunks(validations) do
     for {:error, :illegal_char, char} <- validations, reduce: 0 do
       sum -> sum + @illegal_char_points[char]
     end
   end
 
+  # Part 2 result
   defp score_incomplete_chunks(validations) do
     scores =
       for {:error, :incomplete, stack} <- validations do
@@ -71,8 +68,10 @@ defmodule AdventOfCode.Day10 do
           score -> score * 5 + @incomplete_char_points[char]
         end
       end
-      |> Enum.sort()
 
-    Enum.at(scores, trunc((length(scores) - 1) / 2))
+    # Pick the median value
+    scores
+    |> Enum.sort()
+    |> Enum.at(trunc((length(scores) - 1) / 2))
   end
 end
