5f6bad0562403ed3057d7b81d081d3773d9c3fef
Rockwell Schrock <schrockwell@gmail.com>
2021-12-28 11:25:45 -0500

    2021 day 23 part 2 electric boogaloo

--- 2021/elixir/lib/advent_of_code/day_23.ex
+++ 2021/elixir/lib/advent_of_code/day_23.ex
@@ -1,12 +1,21 @@
 defmodule AdventOfCode.Day23 do
   @behaviour AdventOfCode
+  @a 0
+  @b 1
+  @c 2
+  @d 3
 
   defmodule Pod do
-    defstruct target: nil, position: nil, steps: 0, moved?: false, done?: false
+    defstruct target: nil, position: nil, steps: 0, moved_to_hallway?: false, done?: false
   end
 
   defmodule Burrow do
-    defstruct pods: %{}, total_energy: 0, room_depth: 2
+    @a 0
+    @b 1
+    @c 2
+    @d 3
+
+    defstruct pods: %{}, total_energy: 0, room_depth: nil
 
     require IEx
 
@@ -35,10 +44,10 @@ defmodule AdventOfCode.Day23 do
     def organize_pod(burrow, move, min_energy \\ 999_999_999) do
       next_burrow = perform_move(burrow, move)
 
-      # if not burrow_valid?(next_burrow) do
-      #   IEx.pry()
-      #   raise "invalid burrow"
-      # end
+      if not burrow_valid?(next_burrow) do
+        IEx.pry()
+        raise "invalid burrow"
+      end
 
       next_energy = next_burrow.total_energy
 
@@ -50,7 +59,7 @@ defmodule AdventOfCode.Day23 do
         burrow_complete?(next_burrow) ->
           # Check if we have a new winner
           if next_energy < min_energy do
-            # IO.inspect(next_energy, label: "new min")
+            IO.inspect(next_energy, label: "new min")
             next_energy
           else
             min_energy
@@ -81,21 +90,11 @@ defmodule AdventOfCode.Day23 do
     end
 
     def next_moves_for_pod(burrow, pod) do
-      cond do
-        pod.done? ->
-          # Pod is in place; nothing to figure out
-          []
-
-        # Pod is in the back of its target room, so it will never move
-        back_of_target_room?(pod) ->
-          []
-
-        # Pod is in the back of a room and the front is also occupied
-        back_of_a_room?(pod) and front_is_occupied?(burrow, pod) ->
-          []
-
-        true ->
-          get_next_moves(burrow, pod)
+      if pod.done? do
+        # Pod is in place; no moves to consider
+        []
+      else
+        get_next_moves(burrow, pod)
       end
     end
 
@@ -103,13 +102,18 @@ defmodule AdventOfCode.Day23 do
     # (That is, once any amphipod starts moving, any other amphipods currently in the hallway are locked in
     # place and will not move again until they can move fully into a room.)
     def get_next_moves(burrow, %Pod{position: {:room, room, _} = room_pos} = pod) do
-      # Get hallway positions
-      for index <- reachable_hallway_indexes_from_room(burrow, room) do
-        %{
-          position: {:hallway, index},
-          steps: steps_between(room_pos, {:hallway, index}),
-          pod: pod
-        }
+      # Pod is blocked by other pods in the room's doorway
+      if room_occupied_ahead?(burrow, pod) do
+        []
+      else
+        # Get hallway positions
+        for index <- reachable_hallway_indexes_from_room(burrow, room) do
+          %{
+            position: {:hallway, index},
+            steps: steps_between(room_pos, {:hallway, index}),
+            pod: pod
+          }
+        end
       end
     end
 
@@ -121,17 +125,46 @@ defmodule AdventOfCode.Day23 do
       # leaves the room. (For example, an Amber amphipod will not move from the hallway into the right
       # three rooms, and will only move into the leftmost room if that room is empty or if it only contains
       # other Amber amphipods.)
-      cond do
-        target_room_back_reachable?(burrow, pod) ->
-          dest = {:room, pod.target, 1}
-          [%{position: dest, steps: steps_between(dest, pod.position), pod: pod}]
+      can_walk? = hallway_clear_to_room_doorway?(burrow, pod)
+      target_position = position_in_target_room(burrow, pod)
 
-        target_room_front_reachable?(burrow, pod) ->
-          dest = {:room, pod.target, 0}
-          [%{position: dest, steps: steps_between(dest, pod.position), pod: pod}]
+      if can_walk? && target_position do
+        [
+          %{
+            position: target_position,
+            steps: steps_between(target_position, pod.position),
+            pod: pod
+          }
+        ]
+      else
+        []
+      end
+    end
 
-        true ->
-          []
+    def position_in_target_room(burrow, %Pod{target: target}) do
+      0..(burrow.room_depth - 1)
+      |> Enum.map(fn i -> burrow.pods[{:room, target, i}] end)
+      |> case do
+        [nil, nil] ->
+          {:room, target, 1}
+
+        [nil, %Pod{target: ^target}] ->
+          {:room, target, 0}
+
+        [nil, nil, nil, nil] ->
+          {:room, target, 3}
+
+        [nil, nil, nil, %Pod{target: ^target}] ->
+          {:room, target, 2}
+
+        [nil, nil, %Pod{target: ^target}, %Pod{target: ^target}] ->
+          {:room, target, 1}
+
+        [nil, %Pod{target: ^target}, %Pod{target: ^target}, %Pod{target: ^target}] ->
+          {:room, target, 0}
+
+        _ ->
+          nil
       end
     end
 
@@ -140,8 +173,8 @@ defmodule AdventOfCode.Day23 do
         move.pod
         | position: move.position,
           steps: move.pod.steps + move.steps,
-          moved?: true,
-          done?: move.pod.moved?
+          moved_to_hallway?: true,
+          done?: move.pod.moved_to_hallway?
       }
 
       next_pods =
@@ -152,36 +185,19 @@ defmodule AdventOfCode.Day23 do
       %{burrow | pods: next_pods, total_energy: burrow.total_energy + move_energy(move)}
     end
 
-    def target_room_back_reachable?(burrow, pod) do
-      target_room_reachable?(burrow, pod) and
-        not Map.has_key?(burrow.pods, {:room, pod.target, 0}) and
-        not Map.has_key?(burrow.pods, {:room, pod.target, 1})
-    end
-
-    def target_room_front_reachable?(burrow, %{target: target} = pod) do
-      with true <- target_room_reachable?(burrow, pod),
-           %{target: ^target} <- burrow.pods[{:room, pod.target, 1}],
-           nil <- burrow.pods[{:room, pod.target, 0}] do
-        true
-      else
-        _ -> false
-      end
-    end
-
-    def target_room_reachable?(burrow, %Pod{position: {:hallway, index}} = pod) do
+    def hallway_clear_to_room_doorway?(burrow, %Pod{position: {:hallway, index}} = pod) do
       hallway_range = index..room_hallway_index(pod.target)
 
       Enum.all?(hallway_range, fn i -> burrow.pods[{:hallway, i}] in [nil, pod] end)
     end
 
-    def back_of_target_room?(%Pod{position: {:room, room, 1}, target: room}), do: true
-    def back_of_target_room?(_pod), do: false
+    def room_occupied_ahead?(_burrow, %Pod{position: {:hallway, _}}), do: false
+    def room_occupied_ahead?(_burrow, %Pod{position: {:room, _room, 0}}), do: false
 
-    def back_of_a_room?(%Pod{position: {:room, _, 1}}), do: true
-    def back_of_a_room?(_), do: false
-
-    def front_is_occupied?(burrow, pod) do
-      Map.has_key?(burrow.pods, {:room, room(pod), 0})
+    def room_occupied_ahead?(burrow, %Pod{position: {:room, room, index}}) do
+      Enum.any?((index - 1)..0, fn i ->
+        Map.has_key?(burrow.pods, {:room, room, i})
+      end)
     end
 
     def room_complete?(burrow, room) do
@@ -196,26 +212,23 @@ defmodule AdventOfCode.Day23 do
     def room(%Pod{position: {:room, num, _}}), do: num
 
     def steps_between(
-          {:room, room, _front_or_back} = room_position,
-          {:hallway, index} = _hallway_position
+          {:room, room, room_index},
+          {:hallway, hallway_index}
         ) do
-      abs(room_hallway_index(room) - index) + steps_out_of_room(room_position)
+      abs(room_hallway_index(room) - hallway_index) + room_index + 1
     end
-
-    def steps_out_of_room({:room, _, 0}), do: 1
-    def steps_out_of_room({:room, _, 1}), do: 2
 
     def burrow_complete?(burrow) do
       Enum.all?(0..3, &room_complete?(burrow, &1))
     end
 
-    def move_energy(%{pod: %{target: 0}, steps: steps}), do: steps
-    def move_energy(%{pod: %{target: 1}, steps: steps}), do: steps * 10
-    def move_energy(%{pod: %{target: 2}, steps: steps}), do: steps * 100
-    def move_energy(%{pod: %{target: 3}, steps: steps}), do: steps * 1000
+    def move_energy(%{pod: %{target: @a}, steps: steps}), do: steps
+    def move_energy(%{pod: %{target: @b}, steps: steps}), do: steps * 10
+    def move_energy(%{pod: %{target: @c}, steps: steps}), do: steps * 100
+    def move_energy(%{pod: %{target: @d}, steps: steps}), do: steps * 1000
 
     def burrow_valid?(burrow) do
-      map_size(burrow.pods) == 8
+      map_size(burrow.pods) == 4 * burrow.room_depth
     end
 
     def display(burrow) do
@@ -229,31 +242,26 @@ defmodule AdventOfCode.Day23 do
 
       IO.puts("#")
 
-      IO.write("###")
+      for i <- 0..(burrow.room_depth - 1) do
+        if i == 0, do: IO.write("###"), else: IO.write("  #")
 
-      for r <- 0..3 do
-        burrow |> display_at_position({:room, r, 0}) |> IO.write()
-        IO.write("#")
+        for r <- 0..3 do
+          burrow |> display_at_position({:room, r, i}) |> IO.write()
+          IO.write("#")
+        end
+
+        if i == 0, do: IO.puts("##"), else: IO.puts("  ")
       end
 
-      IO.puts("##")
-      IO.write("  #")
-
-      for r <- 0..3 do
-        burrow |> display_at_position({:room, r, 1}) |> IO.write()
-        IO.write("#")
-      end
-
-      IO.puts("  ")
       IO.puts("  #########  ")
     end
 
     defp display_at_position(burrow, position) do
       case burrow.pods[position] do
-        %Pod{target: 0} -> "A"
-        %Pod{target: 1} -> "B"
-        %Pod{target: 2} -> "C"
-        %Pod{target: 3} -> "D"
+        %Pod{target: @a} -> "A"
+        %Pod{target: @b} -> "B"
+        %Pod{target: @c} -> "C"
+        %Pod{target: @d} -> "D"
         _ -> "."
       end
     end
@@ -263,41 +271,71 @@ defmodule AdventOfCode.Day23 do
     # Sample burrow
     #
     # burrow = %Burrow{
+    #   room_depth: 2,
     #   pods: %{
-    #     {:room, 0, 0} => %Pod{target: 1, position: {:room, 0, 0}},
-    #     {:room, 0, 1} => %Pod{target: 0, position: {:room, 0, 1}},
-    #     {:room, 1, 0} => %Pod{target: 2, position: {:room, 1, 0}},
-    #     {:room, 1, 1} => %Pod{target: 3, position: {:room, 1, 1}},
-    #     {:room, 2, 0} => %Pod{target: 1, position: {:room, 2, 0}},
-    #     {:room, 2, 1} => %Pod{target: 2, position: {:room, 2, 1}},
-    #     {:room, 3, 0} => %Pod{target: 3, position: {:room, 3, 0}},
-    #     {:room, 3, 1} => %Pod{target: 0, position: {:room, 3, 1}}
+    #     {:room, 0, 0} => %Pod{target: @b, position: {:room, 0, 0}},
+    #     {:room, 0, 1} => %Pod{target: @a, position: {:room, 0, 1}, done?: true},
+    #     {:room, 1, 0} => %Pod{target: @c, position: {:room, 1, 0}},
+    #     {:room, 1, 1} => %Pod{target: @d, position: {:room, 1, 1}},
+    #     {:room, 2, 0} => %Pod{target: @b, position: {:room, 2, 0}},
+    #     {:room, 2, 1} => %Pod{target: @c, position: {:room, 2, 1}, done?: true},
+    #     {:room, 3, 0} => %Pod{target: @d, position: {:room, 3, 0}},
+    #     {:room, 3, 1} => %Pod{target: @a, position: {:room, 3, 1}}
     #   }
     # }
 
-    # Problem burrow
+    # Part 1 burrow
     #
-    # burrow = %Burrow{
-    #   pods: %{
-    #     {:room, 0, 0} => %Pod{target: 3, position: {:room, 0, 0}},
-    #     {:room, 0, 1} => %Pod{target: 1, position: {:room, 0, 1}},
-    #     {:room, 1, 0} => %Pod{target: 1, position: {:room, 1, 0}},
-    #     {:room, 1, 1} => %Pod{target: 3, position: {:room, 1, 1}},
-    #     {:room, 2, 0} => %Pod{target: 0, position: {:room, 2, 0}},
-    #     {:room, 2, 1} => %Pod{target: 0, position: {:room, 2, 1}},
-    #     {:room, 3, 0} => %Pod{target: 2, position: {:room, 3, 0}},
-    #     {:room, 3, 1} => %Pod{target: 2, position: {:room, 3, 1}}
-    #   }
-    # }
+    _burrow_a = %Burrow{
+      room_depth: 2,
+      pods: %{
+        {:room, 0, 0} => %Pod{target: @d, position: {:room, 0, 0}},
+        {:room, 0, 1} => %Pod{target: @b, position: {:room, 0, 1}},
+        {:room, 1, 0} => %Pod{target: @b, position: {:room, 1, 0}},
+        {:room, 1, 1} => %Pod{target: @d, position: {:room, 1, 1}},
+        {:room, 2, 0} => %Pod{target: @a, position: {:room, 2, 0}},
+        {:room, 2, 1} => %Pod{target: @a, position: {:room, 2, 1}},
+        {:room, 3, 0} => %Pod{target: @c, position: {:room, 3, 0}},
+        {:room, 3, 1} => %Pod{target: @c, position: {:room, 3, 1}}
+      }
+    }
 
-    # start_time = System.monotonic_time(:second)
-    # answer_a = Burrow.organize_async(burrow)
-    # end_time = System.monotonic_time(:second)
-    # IO.puts("Completed in #{end_time - start_time} seconds")
+    # Part 2 burrow
+    burrow_b = %Burrow{
+      room_depth: 4,
+      pods: %{
+        # Room 0
+        {:room, 0, 0} => %Pod{target: @d, position: {:room, 0, 0}},
+        {:room, 0, 1} => %Pod{target: @d, position: {:room, 0, 1}},
+        {:room, 0, 2} => %Pod{target: @d, position: {:room, 0, 2}},
+        {:room, 0, 3} => %Pod{target: @b, position: {:room, 0, 3}},
+        # Room 1
+        {:room, 1, 0} => %Pod{target: @b, position: {:room, 1, 0}},
+        {:room, 1, 1} => %Pod{target: @c, position: {:room, 1, 1}},
+        {:room, 1, 2} => %Pod{target: @b, position: {:room, 1, 2}},
+        {:room, 1, 3} => %Pod{target: @d, position: {:room, 1, 3}},
+        # Room 2
+        {:room, 2, 0} => %Pod{target: @a, position: {:room, 2, 0}},
+        {:room, 2, 1} => %Pod{target: @b, position: {:room, 2, 1}},
+        {:room, 2, 2} => %Pod{target: @a, position: {:room, 2, 2}},
+        {:room, 2, 3} => %Pod{target: @a, position: {:room, 2, 3}},
+        # Room 3
+        {:room, 3, 0} => %Pod{target: @c, position: {:room, 3, 0}},
+        {:room, 3, 1} => %Pod{target: @a, position: {:room, 3, 1}},
+        {:room, 3, 2} => %Pod{target: @c, position: {:room, 3, 2}},
+        {:room, 3, 3} => %Pod{target: @c, position: {:room, 3, 3}}
+      }
+    }
 
-    # Hardcoding answers so that GitHub actions doesn't have to run this challenge
+    # Hardcoding this answer since it takes several minutes to run
     answer_a = 17120
+    # answer_a = Burrow.organize_async(burrow_a)
 
-    {answer_a, "todo"}
+    start_time = System.monotonic_time(:second)
+    answer_b = Burrow.organize_async(burrow_b)
+    end_time = System.monotonic_time(:second)
+    IO.puts("Completed in #{end_time - start_time} seconds")
+
+    {answer_a, answer_b}
   end
 end
