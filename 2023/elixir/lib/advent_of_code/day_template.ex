7553facc9530510aa51cf011f83e84eb28ac2eef
Rockwell Schrock <rockwell@schrock.me>
2023-12-03 09:38:02 -0500

    Add new.sh helper script

--- 2023/elixir/lib/advent_of_code/day_template.ex
+++ 2023/elixir/lib/advent_of_code/day_template.ex
@@ -2,6 +2,6 @@ defmodule AdventOfCode.DayTemplate do
   @behaviour AdventOfCode
 
   def run(_basename, _part, _input) do
-    nil
+    0
   end
 end
---
+++ 2023/elixir/new.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Get the day of the month from the current date
+day=$(date +%d)
+
+# Create priv/days/{day}/input.txt and priv/days/{day}/example.txt
+mkdir -p priv/days/$day
+
+printf "0\n---\ninput here" > priv/days/$day/input.txt
+printf "0\n---\nexample here" > priv/days/$day/example.txt
+
+ # Copy the DayTemplate module
+cp lib/advent_of_code/day_template.ex lib/advent_of_code/day_$day.ex
+
+# Replace "DayTemplate" with "Day{day}"
+sed -i '' "s/DayTemplate/Day$day/g" lib/advent_of_code/day_$day.ex
+
+# Open the files
+code priv/days/$day/example.txt
+code priv/days/$day/input.txt
+code lib/advent_of_code/day_$day.ex
+
+echo "Created files for day $day"
