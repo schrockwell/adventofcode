ab65590ecbcd5ed7dab5885459e5d8f127ae8256
Rockwell Schrock <rockwell@schrock.me>
2023-12-06 10:12:05 -0500

    2023 day 5 part 2

--- 2023/elixir/lib/advent_of_code/day_05.ex
+++ 2023/elixir/lib/advent_of_code/day_05.ex
@@ -4,10 +4,7 @@ defmodule AdventOfCode.Day05 do
   @dest_types ["soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
 
   # Part 1
-  def run(_basename, 1, [
-        "seeds: " <> seeds
-        | maps
-      ]) do
+  def run(_basename, 1, ["seeds: " <> seeds | maps]) do
     seeds = seeds |> String.split(" ") |> Enum.map(&String.to_integer/1)
     maps = parse_maps(maps)
 
@@ -21,21 +18,68 @@ defmodule AdventOfCode.Day05 do
   end
 
   # Part 2
-  def run(_basename, 2, [
-        "seeds: " <> seeds
-        | maps
-      ]) do
-    _seed_ranges =
+  def run(_basename, 2, ["seeds: " <> seeds | maps]) do
+    seed_ranges =
       seeds
       |> String.split(" ")
       |> Enum.map(&String.to_integer/1)
       |> Enum.chunk_every(2)
-      |> Enum.map(fn [first, last] -> first..(first + last - 1) end)
+      |> Enum.map(fn [start, count] ->
+        start..(start + count - 1)
+      end)
 
-    # idk
-    0
+    maps = parse_maps(maps)
+
+    # Kick off recursion
+    find_lowest_loc(0, maps, seed_ranges)
   end
 
+  # Part 1 - Walk the transitions from seed to location
+  defp find_next(maps, src_value, dest_type) do
+    Enum.find(maps, fn
+      %{to: ^dest_type, src_range: src_range} -> src_value in src_range
+      _ -> false
+    end)
+    |> case do
+      %{delta: delta} -> src_value + delta
+      nil -> src_value
+    end
+  end
+
+  # Part 2 - Walk the transitions BACKWARDS, from location to seed
+  defp find_prev(maps, dest_value, dest_type) do
+    Enum.find(maps, fn
+      %{to: ^dest_type, dest_range: dest_range} -> dest_value in dest_range
+      _ -> false
+    end)
+    |> case do
+      %{delta: delta} -> dest_value - delta
+      nil -> dest_value
+    end
+  end
+
+  # Part 2
+  defp find_lowest_loc(loc, maps, seed_ranges) do
+    # Walk the transitions backwards, determining the starting seed from the final location
+    seed =
+      Enum.reverse(@dest_types)
+      |> Enum.reduce(loc, fn dest_type, val ->
+        find_prev(maps, val, dest_type)
+      end)
+
+    if Enum.any?(seed_ranges, fn range -> seed in range end) do
+      # We're done!
+      loc
+    else
+      # Display progress, for my own sanity
+      if rem(loc, 100_000) == 0, do: IO.inspect(loc)
+
+      # Keep going
+      find_lowest_loc(loc + 1, maps, seed_ranges)
+    end
+  end
+
+  # Parsing
   defp parse_maps(input) do
     chunks =
       Enum.chunk_by(input, fn line ->
@@ -54,23 +98,10 @@ defmodule AdventOfCode.Day05 do
           from: from,
           to: to,
           src_range: src_start..(src_start + count - 1),
+          dest_range: dest_start..(dest_start + count - 1),
           delta: dest_start - src_start
         }
       end)
     end)
-  end
-
-  defp find_next(maps, src_value, dest_type) do
-    Enum.find(maps, fn
-      %{to: ^dest_type, src_range: src_range} ->
-        src_value in src_range
-
-      _ ->
-        false
-    end)
-    |> case do
-      %{delta: delta} -> src_value + delta
-      nil -> src_value
-    end
   end
 end
